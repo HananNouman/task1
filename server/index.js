c39de81011625f572c2a136e9e7a9a6f89e2c066
alaamigdady <alaamohammad283@gmail.com>
2018-10-05 05:32:38 +0300

    addStudent

--- server/index.js
+++ server/index.js
@@ -6,10 +6,32 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
 const server = app
 .listen(port, () => console.log(`Listening on ${ port }`));
 
 
+
+
+app.post('/addStudent',function(req,res){
+var student = new db.Student(req.body);
+ student.save()
+ .then(item => {
+ res.send("item saved to database");
+ })
+ .catch(err => {
+ res.status(400).send("unable to save to database");
+ });
+
+
+})
+
+
+
 app.get('/', function (req, res) {
-  res.json( 'Welcome to the sudent system!' );
+  res.send( 'Welcome to the student system!' );
 });
