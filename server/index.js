4dcdee3a82e9c9853ff482b5798bbf8ca19e1a0e
alaamigdady <alaamohammad283@gmail.com>
2018-10-05 14:30:39 +0300

    get request

--- server/index.js
+++ server/index.js
@@ -15,23 +15,50 @@ const server = app
 .listen(port, () => console.log(`Listening on ${ port }`));
 
 
+app.get('/', function (req, res) {
+  res.send( 'Welcome to the student system!' );
+});
 
 
 app.post('/addStudent',function(req,res){
-var student = new db.Student(req.body);
- student.save()
- .then(item => {
- res.send("item saved to database");
- })
- .catch(err => {
- res.status(400).send("unable to save to database");
- });
+    var student = new db.Student(req.body);
+    student.save()
+    .then(item => {
+       res.send("item saved to database");
+   })
+    .catch(err => {
+        console.log(err)
+       res.status(400).send("unable to save to database");
+   });
 
 
 })
 
+app.get('/student/:studentId',function(req,res){
+    var studentId = Number(req.params.studentId)
+
+    db.Student.findOne({studentId:studentId})
+    .then(data => {
+        res.send(data)
+    })
+    .catch(err => {
+        console.log(err)
+    })
+
+})
 
 
-app.get('/', function (req, res) {
-  res.send( 'Welcome to the student system!' );
-});
+app.get('/student/:studentName',function(req,res){
+    var studentName = req.params.studentName
+    
+
+    db.Student.find({studentName:studentName})
+    .then(data => {
+        res.send(data)
+    })
+    .catch(err => {
+        console.log(err)
+    })
+
+
+})
